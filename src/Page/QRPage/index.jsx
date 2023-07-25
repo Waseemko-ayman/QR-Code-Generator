6a5cc80d114924ac6cb7f03e079f829477250de1
Waseem A. Abd Elhadi <112069422+Waseemko-ayman@users.noreply.github.com>
2023-07-25 05:49:02 +0300

    Update

--- src/Page/QRPage/index.jsx
+++ src/Page/QRPage/index.jsx
@@ -6,16 +6,20 @@ const QRPage = () => {
 
   const [text, setText] = useState('');
 
+  const handleChange = (e) => {
+    setText(e.target.value);
+  }
+
   return (
     <div className='qr'>
       <h1>QR Code Generator</h1>
       <div className="input-here">
         <p>Enter your text here</p>
-        <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+        <input type="text" value={text} onChange={handleChange} />
       </div>
       <QRCode value={text} style={{ maxWidth: "100%", width: "80%" }}/>
     </div>
   )
 }
 
-export default QRPage
+export default QRPage;
